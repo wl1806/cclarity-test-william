b85df0354fab0bb517ff946dbed515e00b620a99
William <william@whim.sg>
2024-10-15 11:34:03 +0700

    update version

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 /* eslint-disable */
-const withLess = require('@zeit/next-less')
-const withSass = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
-const lessToJS = require('less-vars-to-js')
-const fs = require('fs')
-const path = require('path')
+const withLess = require("@zeit/next-less");
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
+const path = require("path");
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
   fs.readFileSync(
-    path.resolve(__dirname, './src/styles/antd-custom.less'),
-    'utf8'
+    path.resolve(__dirname, "./src/styles/antd-custom.less"),
+    "utf8"
   )
-)
+);
 
 module.exports = {
   env: {
     APP_ENV: process.env.APP_ENV,
-    XENDIT_PUBLISH_API_KEY: process.env.XENDIT_PUBLISH_API_KEY
+    XENDIT_PUBLISH_API_KEY: process.env.XENDIT_PUBLISH_API_KEY,
   },
   ...withCSS({
     cssModules: true,
@@ -27,38 +27,38 @@ module.exports = {
         lessLoaderOptions: {
           javascriptEnabled: true,
           modifyVars: themeVariables,
-          importLoaders: 0
+          importLoaders: 0,
         },
         cssLoaderOptions: {
           importLoaders: 3,
-          localIdentName: '[local]'
+          localIdentName: "[local]",
         },
-        webpack: (config, { isServer }) => {
-          if (isServer) {
-            const antStyles = /antd\/.*?\/style.*?/
-            const origExternals = [...config.externals]
-            config.externals = [
-              (context, request, callback) => {
-                if (request.match(antStyles)) return callback()
-                if (typeof origExternals[0] === 'function') {
-                  origExternals[0](context, request, callback)
-                } else {
-                  callback()
-                }
-              },
-              ...(typeof origExternals[0] === 'function' ? [] : origExternals)
-            ]
+        // webpack: (config, { isServer }) => {
+        //   if (isServer) {
+        //     const antStyles = /antd\/.*?\/style.*?/
+        //     const origExternals = [...config.externals]
+        //     config.externals = [
+        //       (context, request, callback) => {
+        //         if (request.match(antStyles)) return callback()
+        //         if (typeof origExternals[0] === 'function') {
+        //           origExternals[0](context, request, callback)
+        //         } else {
+        //           callback()
+        //         }
+        //       },
+        //       ...(typeof origExternals[0] === 'function' ? [] : origExternals)
+        //     ]
 
-            config.module.rules.unshift({
-              test: antStyles,
-              use: 'null-loader'
-            })
-          } else {
-            config.resolve.alias['@sentry/node'] = '@sentry/browser'
-          }
-          return config
-        }
-      })
-    })
-  })
-}
+        //     config.module.rules.unshift({
+        //       test: antStyles,
+        //       use: 'null-loader'
+        //     })
+        //   } else {
+        //     config.resolve.alias['@sentry/node'] = '@sentry/browser'
+        //   }
+        //   return config
+        // }
+      }),
+    }),
+  }),
+};
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,263 +1,271 @@
-import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint'
+import useBreakpoint from "antd/lib/grid/hooks/useBreakpoint";
 
-import { Avatar, Button, Col, Image, Layout, Row, Typography } from 'antd'
-import './style.css'
-import { WithRouterProps } from 'next/dist/client/with-router'
-import { NextRouter, withRouter } from 'next/router'
-import React from 'react'
-import { connect } from 'react-redux'
+import { Avatar, Button, Col, Image, Layout, Row, Typography } from "antd";
+import "./style.css";
+import { WithRouterProps } from "next/dist/client/with-router";
+import { NextRouter, withRouter } from "next/router";
+import React from "react";
+import { connect } from "react-redux";
 
-import IAUthState from '../../interfaces/states/auth'
-import { ReduxState } from '../../store/reducers'
-import SEO from '../seo'
-import Text from 'antd/lib/typography/Text'
-import LayoutSideMenu from './layout-side-menu'
+import IAUthState from "../../interfaces/states/auth";
+import { ReduxState } from "../../store/reducers";
+import SEO from "../seo";
+import Text from "antd/lib/typography/Text";
+import LayoutSideMenu from "./layout-side-menu";
 
-const { Content, Sider, Header }   = Layout
+const { Content, Sider, Header } = Layout;
 interface IProps extends WithRouterProps {
-  children: React.ReactNode
-  className?: string
-  auth: IAUthState
-  isMobile?: boolean
+  children: React.ReactNode;
+  className?: string;
+  auth: IAUthState;
+  isMobile?: boolean;
 }
 
 interface SIDE_MENU {
-  text: string
-  path: string
-  image: string
-  isActive?: boolean
+  text: string;
+  path: string;
+  image: string;
+  isActive?: boolean;
 }
 
 const sideMenu: SIDE_MENU[] = [
-{
-  image:'/images/btn_magic.png',
-  path:'/#',
-  text:'Magic Write',
-  isActive: true
-},
-{
-  image:'/images/btn_post.png',
-  path:'/#',
-  text:'Post'
-},
-{
-  image:'/images/btn_analytic.png',
-  path:'/#',
-  text:'Analysis'
-},
-{
-  image:'/images/btn_idea.png',
-  path:'/#',
-  text:'Ideas'
-},
-{
-  image:'/images/btn_template.png',
-  path:'/#',
-  text:'Templates'
-},
-{
-  image:'/images/btn_scratch.png',
-  path:'/#',
-  text:'Scratch'
-}
-]
-
+  {
+    image: "/images/btn_magic.png",
+    path: "/#",
+    text: "Magic Write",
+    isActive: true,
+  },
+  {
+    image: "/images/btn_post.png",
+    path: "/#",
+    text: "Post",
+  },
+  {
+    image: "/images/btn_analytic.png",
+    path: "/#",
+    text: "Analysis",
+  },
+  {
+    image: "/images/btn_idea.png",
+    path: "/#",
+    text: "Ideas",
+  },
+  {
+    image: "/images/btn_template.png",
+    path: "/#",
+    text: "Templates",
+  },
+  {
+    image: "/images/btn_scratch.png",
+    path: "/#",
+    text: "Scratch",
+  },
+];
 
 const sideMenuBot: SIDE_MENU[] = [
   {
-    image:'/images/btn_billing.png',
-    path:'/#',
-    text:'Billing'
+    image: "/images/btn_billing.png",
+    path: "/#",
+    text: "Billing",
   },
   {
-    image:'/images/btn_idea.png',
-    path:'/#',
-    text:'Payment'
+    image: "/images/btn_idea.png",
+    path: "/#",
+    text: "Payment",
   },
   {
-    image:'/images/help.png',
-    path:'/#',
-    text:'Help'
+    image: "/images/help.png",
+    path: "/#",
+    text: "Help",
   },
   {
-    image:'/images/btn_time.png',
-    path:'/#',
-    text:'3d left'
-  }
-]
+    image: "/images/btn_time.png",
+    path: "/#",
+    text: "3d left",
+  },
+];
 
 interface IState {
   openSidebar: boolean;
 }
 
 class LayoutComponent extends React.Component<IProps, IState> {
-  static defaultProps: IProps
+  static defaultProps: IProps;
 
   constructor(props: IProps) {
-    super(props)
+    super(props);
     this.state = {
-      openSidebar: false
-    }
+      openSidebar: false,
+    };
   }
 
-  handleClickOutside
+  handleClickOutside;
 
   componentDidMount() {
-    const siderComponent = document.getElementById('sider-component');
-    const burgerComponent = document.getElementById('burger');
+    const siderComponent = document.getElementById("sider-component");
+    const burgerComponent = document.getElementById("burger");
     this.handleClickOutside = (event) => {
-      if (!siderComponent?.contains(event.target) && !burgerComponent?.contains(event.target)) {
-        this.setState({openSidebar:false})
+      if (
+        !siderComponent?.contains(event.target) &&
+        !burgerComponent?.contains(event.target)
+      ) {
+        this.setState({ openSidebar: false });
       }
     };
 
-    document.body.addEventListener('click', this.handleClickOutside);
+    document.body.addEventListener("click", this.handleClickOutside);
   }
 
   componentWillUnmount() {
-    document.body.removeEventListener('click', this.handleClickOutside);
+    document.body.removeEventListener("click", this.handleClickOutside);
   }
 
   render() {
-    const {
-      children,
-      className,
-      isMobile
-    } = this.props
+    const { children, className, isMobile } = this.props;
     return (
       <Layout className={className}>
         <SEO />
-        <Header
-        className={`header ${this.state.openSidebar?'blur':''}`}>
-          <Row align='middle' justify='space-between' wrap={false} style={{ height: '100%' }}>
+        <Header className={`header ${this.state.openSidebar ? "blur" : ""}`}>
+          <Row
+            align="middle"
+            justify="space-between"
+            wrap={false}
+            style={{ height: "100%" }}
+          >
             <Col>
-            <Row className=''>
-              <Col>
-                {!isMobile?<></>:
-                  <Image 
-                  id='burger'
-                  onClick={()=>this.setState({openSidebar:true})}
-                className='burger'                  
-                wrapperClassName='burger-wrapper'
-                  src={'/images/burger.png'}
-                  preview={false}
+              <Row className="">
+                <Col>
+                  {!isMobile ? (
+                    <></>
+                  ) : (
+                    <div onClick={() => this.setState({ openSidebar: true })}>
+                      <Image
+                        id="burger"
+                        className="burger"
+                        wrapperClassName="burger-wrapper"
+                        src={"/images/burger.png"}
+                        preview={false}
+                      />
+                    </div>
+                  )}
+                </Col>
+                <Col>
+                  <Image
+                    wrapperClassName="logo-wrapper"
+                    className="logo-inner"
+                    src="/images/logo.png"
+                    preview={false}
                   />
-                }
-              </Col>
-              <Col>
-                <Image 
-                wrapperClassName='logo-wrapper'
-                className='logo-inner'
-                  src='/images/logo.png'
-                  preview={false}
-                />
-              </Col>
-              <Col>
-                <Text className='text-family-open-sans trial-text ph-1'>
-                  TRIAL                  
-                </Text>
-              </Col>
-            </Row>
+                </Col>
+                <Col>
+                  <Text className="text-family-open-sans trial-text ph-1">
+                    TRIAL
+                  </Text>
+                </Col>
+              </Row>
             </Col>
             <Col>
-              <Avatar
-                size={40}
-                src={this.props.auth.data?.image}
-              />
+              <Avatar size={40} src={this.props.auth.data?.image} />
             </Col>
           </Row>
         </Header>
         <Layout>
-          {isMobile?
-          <>
-          <Sider id='sider-component' className={`sider-wrap ph-1 ${this.state.openSidebar?'sider-wrap-active':'' }`}>
-            <Col  span={24} className='sider-wrap-col'>
-              <Row>
-                <Col span={24}>
-                  {sideMenu.map(e=>{
-                    return <LayoutSideMenu 
-                    key={e.text} 
-                    image={e.image}
-                    path={e.path}
-                    text={e.text}
-                    isActive={e.isActive}
-                  />
-                  })}
+          {isMobile ? (
+            <>
+              <Sider
+                id="sider-component"
+                className={`sider-wrap ph-1 ${
+                  this.state.openSidebar ? "sider-wrap-active" : ""
+                }`}
+              >
+                <Col span={24} className="sider-wrap-col">
+                  <Row>
+                    <Col span={24}>
+                      {sideMenu.map((e) => {
+                        return (
+                          <LayoutSideMenu
+                            key={e.text}
+                            image={e.image}
+                            path={e.path}
+                            text={e.text}
+                            isActive={e.isActive}
+                          />
+                        );
+                      })}
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={24}>
+                      <LayoutSideMenu
+                        key={"More"}
+                        image={"/images/more.png"}
+                        path={"/#"}
+                        text={"More"}
+                        isActive={false}
+                      />
+                    </Col>
+                  </Row>
                 </Col>
-              </Row>
-              <Row>
-
-                <Col span={24}>
-                <LayoutSideMenu 
-                    key={"More"} 
-                    image={'/images/more.png'}
-                    path={'/#'}
-                    text={'More'}
-                    isActive={false}
-                  />
-                </Col>
-                
-              </Row>
-            </Col>
-          </Sider>
-
-          </>:<Sider className='sider-wrap ph-1'>
-            <Col  span={24} className='sider-wrap-col'>
-              <Row>
-                <Col span={24}>
-                  {sideMenu.map(e=>{
-                    return <LayoutSideMenu 
-                    key={e.text} 
-                    image={e.image}
-                    path={e.path}
-                    text={e.text}
-                    isActive={e.isActive}
-                  />
-                  })}
-                </Col>
-              </Row>
-              <Row>
-
-                <Col span={24}>
-                  {sideMenuBot.map(e=>{
-                    return <LayoutSideMenu 
-                    key={e.text} 
-                    image={e.image}
-                    path={e.path}
-                    text={e.text}
-                    isActive={e.isActive}
-                  />
-                  })}
-                </Col>
-                
-              </Row>
-            </Col>
-          </Sider>
-  }
-          <Content className={this.state.openSidebar?'blur':''}>
-                {children}
+              </Sider>
+            </>
+          ) : (
+            <Sider className="sider-wrap ph-1">
+              <Col span={24} className="sider-wrap-col">
+                <Row>
+                  <Col span={24}>
+                    {sideMenu.map((e) => {
+                      return (
+                        <LayoutSideMenu
+                          key={e.text}
+                          image={e.image}
+                          path={e.path}
+                          text={e.text}
+                          isActive={e.isActive}
+                        />
+                      );
+                    })}
+                  </Col>
+                </Row>
+                <Row>
+                  <Col span={24}>
+                    {sideMenuBot.map((e) => {
+                      return (
+                        <LayoutSideMenu
+                          key={e.text}
+                          image={e.image}
+                          path={e.path}
+                          text={e.text}
+                          isActive={e.isActive}
+                        />
+                      );
+                    })}
+                  </Col>
+                </Row>
+              </Col>
+            </Sider>
+          )}
+          <Content className={this.state.openSidebar ? "blur" : ""}>
+            {children}
           </Content>
-
+        </Layout>
       </Layout>
-      </Layout>
-    )
+    );
   }
 }
 
 LayoutComponent.defaultProps = {
   children: undefined,
   auth: {} as IAUthState,
-  router: {} as NextRouter
-}
+  router: {} as NextRouter,
+};
 
 const mapStateToProps = (state: ReduxState) => ({
-  auth: state.auth
-})
+  auth: state.auth,
+});
 
-const mapDispatchToProps = (dispatch: any) => ({
-})
+const mapDispatchToProps = (dispatch: any) => ({});
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(withRouter(LayoutComponent))
+)(withRouter(LayoutComponent));
